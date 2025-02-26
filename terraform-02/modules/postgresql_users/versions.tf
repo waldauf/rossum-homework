47b39523fc03205fb446c1416a591e2f9c60387f
waldauf <w@w.cz>
2025-02-26 23:36:43 +0100

    second task - done

---
+++ terraform-02/modules/postgresql_users/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.7"
+  required_providers {
+    # https://registry.terraform.io/providers/cyrilgdn/postgresql/latest/docs
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "~> 1.21.0"
+    }
+    # https://registry.terraform.io/providers/hashicorp/random/latest
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
+
+provider "postgresql" {
+  host     = var.postgres_host
+  port     = var.postgres_port
+  username = var.postgres_root
+  password = var.postgres_root_password
+  sslmode  = "disable"
+}
